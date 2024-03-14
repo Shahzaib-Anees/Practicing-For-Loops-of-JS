1541909e1f443b5b2ba053bcfd279a442e172535
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-03-14 23:32:42 +0500

    Series Counting Added

---
+++ Question05-Counting-Series-using-Loops/app.js
@@ -0,0 +1,37 @@
+// Forward Counting 
+document.write("<h3>Forward Counting</h3>")
+for (var a = 1; a < 16 ; a++){
+    document.write(`
+    <span>${a} ,</span>`)
+}
+
+// Reverse Counting 
+document.write("<h3>Reverse Counting</h3>")
+for (var a = 10; a >= 0 ; a--){
+    document.write(`
+    <span>${a} ,</span>`)
+}
+
+
+// Even Number Counting 
+document.write("<h3>Even Number Counting</h3>")
+for (var a = 0; a <= 20 ; a+=2){
+    document.write(`
+    <span>${a} ,</span>`)
+}
+
+// ODD Number Counting 
+document.write("<h3>ODD Number Counting</h3>")
+for (var a = 1; a <= 20 ; a+=2){
+    document.write(`
+    <span>${a} ,</span>`)
+}
+
+
+// Series Counting 
+document.write("<h3>Series Counting</h3>")
+for (var a = 2000; a <= 20000 ; a += 2000){
+    document.write(`
+    <span>${a} ,</span>`)
+}
+
